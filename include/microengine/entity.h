02aa1db5be7f6830960e9228c8544905769ddf7f
nixii <161391079+nixii@users.noreply.github.com>
2026-04-25 13:47:39 -0700

    max entity

--- include/microengine/entity.h
+++ include/microengine/entity.h
@@ -2,10 +2,13 @@
 #ifndef MICROENGINE_ENTITY_H
 #define MICROENGINE_ENTITY_H
 
+#define ME_MAX_ENTITIES 4096
+
 typedef int me_entity;
 #define ME_NIL_ENTITY -1
 
 #ifndef MICROENGINE_NOSTRIP
+    #define MAX_ENTITIES ME_MAX_ENTITIES
     #define entity me_entity
     #define NIL_ENTITY ME_NIL_ENTITY
 #endif
