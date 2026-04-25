4ccc9876c6ea9f9096fe221db8d8d21d1c3625f0
nixii <161391079+nixii@users.noreply.github.com>
2026-04-25 13:41:28 -0700

    invalid entity sentinel

--- include/microengine/entity.h
+++ include/microengine/entity.h
@@ -3,9 +3,11 @@
 #define MICROENGINE_ENTITY_H
 
 typedef int me_entity;
+#define ME_NIL_ENTITY -1
 
 #ifndef MICROENGINE_NOSTRIP
     #define entity me_entity
+    #define NIL_ENTITY ME_NIL_ENTITY
 #endif
 
 #endif // MICROENGINE_ENTITY_H
