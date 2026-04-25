c13a3f8db3f0d1fff5789a57e3e6a1d875647131
nixii <161391079+nixii@users.noreply.github.com>
2026-04-25 13:52:07 -0700

    components x-macro

---
+++ include/microengine/components.h
@@ -0,0 +1,9 @@
+
+#ifndef MICROENGINE_COMPONENTS_H
+#define MICROENGINE_COMPONENTS_H
+
+// X-macro for components.
+#define ME_COMPONENTS \
+    X(transform, me_transform)
+
+#endif // MICROENGINE_COMPONENTS_H
--- include/microengine/scene.h
+++ include/microengine/scene.h
@@ -2,8 +2,27 @@
 #ifndef MICROENGINE_SCENE_H
 #define MICROENGINE_SCENE_H
 
+#include "microengine/entity.h"
+#include "microengine/components.h"
+#include <stdint.h>
+
 typedef struct me_scene {
-    
+    uint8_t alive[ME_MAX_ENTITIES];
+
+    // add components X macro
+#define X(name, type)\
+    uint8_t name##_has[ME_MAX_ENTITIES];\
+    type name##_components[ME_MAX_ENTITIES];
+
+    ME_COMPONENTS
+
+#undef X
 } me_scene;
+
+// spawn a brand new entity
+me_entity me_scene_spawn();
+
+// destroy an entity
+void me_scene_destroy(me_entity e);
 
 #endif // MICROENGINE_SCENE_H
