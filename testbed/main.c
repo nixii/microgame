ce6ec61fe001fb2f14d1b6359e5ccfbda9059ddd
nixii <161391079+nixii@users.noreply.github.com>
2026-04-26 10:34:49 -0700

    simpler test for changing later

--- testbed/main.c
+++ testbed/main.c
@@ -8,40 +8,21 @@
 
 int main(void)
 {
-    // actual random seed
-    srand(time(NULL));
-
+    
     // create the game
     microgame *g = game_new(600, 400, 60, "hi world");
     scene *s = scene_new();
 
-    // meshes
-    int numMeshes = 100;
-    mesh *meshes[numMeshes];
-
-    for (int i = 0; i < numMeshes; i++) {
-        entity e = scene_spawn(s);
-        meshes[i] = scene_attach_mesh(s, e, mesh_new(rgb_rand(), 3, (vec3[]){
-            vec3_new(rand() % 600, rand() % 400, 0),
-            vec3_new(rand() % 600, rand() % 400, 0),
-            vec3_new(rand() % 600, rand() % 400, 0)
-        }));
-    }
-
+    // set the scene
     game_set_scene(g, s);
     int frame = 0;
 
+    // update
     while (game_still_running(g)) {
-
-        for (int i = 0; i < numMeshes; i++) {
-            meshes[i]->verts[0] = vec3_new(rand() % 600, rand() % 400, 0);
-            meshes[i]->verts[1] = vec3_new(rand() % 600, rand() % 400, 0);
-            meshes[i]->verts[2] = vec3_new(rand() % 600, rand() % 400, 0);
-        }
-
         game_update(g);
         frame++;
     }
 
+    // end
     game_destroy(g);
 }
