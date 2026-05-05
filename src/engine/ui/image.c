bf9df5406c0d49a7696deca5bea93d98124c9137
nixii <161391079+nixii@users.noreply.github.com>
2026-05-05 09:35:57 -0700

    finished a todo

--- src/engine/ui/image.c
+++ src/engine/ui/image.c
@@ -14,7 +14,6 @@ void ui_image_destroy(ui_image *i) {
     free(i);
 }
 
-// TODO: scale the image
 // render a ui image
 void ui_image_render(ui_image *i, renderer *r, int x, int y, int w, int h, color overlay) {
     renderer_render_image_stretch(r, x, y, w, h, i->image->width, i->image->height, i->image->pixels, overlay);
