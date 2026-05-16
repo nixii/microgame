8606f79a6c6f7bdfd0510ccd8ee481cf6c24171d
nixii <161391079+nixii@users.noreply.github.com>
2026-05-16 11:20:05 -0700

    added fov effect!

--- testgame1/main.c
+++ testgame1/main.c
@@ -37,6 +37,8 @@ int main(void) {
         update_movement(sc, player);
         check_dialogue_visibility(sc, player, npc);
         game_update(game);
+        float targetFov = (PI / 2) + LENGTH(MUL_COMPONENTS(get_velocity(sc, player)->velocity, vec3_new(1, 0, 1))) / 20;
+        sc->camera.fov += (targetFov - sc->camera.fov) * 0.2f;
     }
 
     // destroy the game
