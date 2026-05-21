4bed3e0a03479b9a4e70567254cb74f83592651a
nixii <161391079+nixii@users.noreply.github.com>
2026-05-20 20:13:45 -0700

    work on the parser

--- editor/lang/parser.c
+++ editor/lang/parser.c
@@ -8,6 +8,9 @@
 DA_DEFINE(ms_nodes, ms_node)
 DA_DEFINE(ms_names, char*)
 
+// length
+#define MS_LEN(a) (sizeof(a) / sizeof((a)[0]))
+
 // different queries
 ms_token_type _ANY_PARAMETER[] = {
     MS_TT_BOOL,
@@ -48,6 +51,24 @@ static inline ms_token ms_ast_next(ms_ast *ast, ms_tokens *tokens) {
     return tokens->data[ast->curPos++];
 }
 
+// get the next of a type
+static inline ms_token *ms_ast_next_of(ms_ast *ast, ms_tokens *tokens, ms_token_type type) {
+    if (tokens->data[ast->curPos].type == type)
+        return &(tokens->data[ast->curPos++]);
+    return NULL;
+}
+static inline ms_token *ms_ast_next_of_any(ms_ast *ast, ms_tokens *tokens, size_t numTypes, ms_token_type *types) {
+    if (ast->curPos >= (size_t)tokens->length) return NULL;
+    ms_token *next = &(tokens->data[ast->curPos]);
+    for (size_t i = 0; i < numTypes; i++) {
+        if (next->type == types[i]) {
+            ast->curPos++;
+            return next;
+        }
+    }
+    return NULL;
+}
+
 // expect of a type
 ms_token ms_ast_expect(ms_ast *ast, ms_tokens *tokens, ms_token_type type) {
     ms_token t = ms_ast_next(ast, tokens);
@@ -58,29 +79,22 @@ ms_token ms_ast_expect(ms_ast *ast, ms_tokens *tokens, ms_token_type type) {
     return t;
 }
 
-// expect any of these types
-ms_token ms_ast_expect_any(ms_ast *ast, ms_tokens *tokens, size_t numTypes, ms_token_type *types) {
-    for (size_t i = 0; i < numTypes; i++) {
-        ms_token t = tokens->data[ast->curPos];
-        if (t.type == types[i]) {
-            ast->curPos++;
-            return t;
-        }
-    }
-    fprintf(stderr, "expected a token of a group of types, found none.");
-    exit(1);
-}
-
-static inline ms_token ms_ast_expect_parameter(ms_ast *ast, ms_tokens *tokens) {
-    return ms_ast_expect_any(ast, tokens, sizeof(_ANY_PARAMETER) / sizeof(ms_token_type), _ANY_PARAMETER);
-}
-
-
 
 
 ///////////////////////
 // HANDLE COMMANDS
-ms_node *ms_ast_parse_echo(ms_ast *ast, ms_tokens *tokens) {}
+ms_node *ms_ast_parse_echo(ms_ast *ast, ms_tokens *tokens) {
+    ms_node *cmd = ms_node_new(MS_NT_CALL, (ms_node_value){ .call = { .funcName = "echo", .firstParam = NULL } });
+    ms_token *paramToken = ms_ast_next_of_any(ast, tokens, MS_LEN(_ANY_PARAMETER), _ANY_PARAMETER);
+    ms_node *param = ms_node_new(MS_NT_PARAM, (ms_node_value){ .param = { .tok = *paramToken, .nextParam = NULL } });
+    cmd->value.call.firstParam = param;
+    while ((paramToken = ms_ast_next_of_any(ast, tokens, MS_LEN(_ANY_PARAMETER), _ANY_PARAMETER)) != NULL) {
+        param->value.param.nextParam = ms_node_new(MS_NT_PARAM, (ms_node_value){ .param = { .tok = *paramToken, .nextParam = NULL } });
+        param = param->value.param.nextParam;
+        printf("param. %s\n", param->value.param.tok.value.chars);
+    }
+    return cmd;
+}
 
 
 
@@ -111,6 +125,13 @@ static const _ms_command_entry _ms_command_table[] = {
 
 // get a whole block
 ms_node *ms_ast_get_next_block(ms_ast *ast, ms_tokens *tokens) {
+    ms_token cmd = ms_ast_expect(ast, tokens, MS_TT_KEYWORD);
+    for (size_t i = 0; i < MS_COMMAND_COUNT; i++) {
+        if (strcmp(cmd.value.chars, _ms_command_table[i].keyword) == 0)
+            return _ms_command_table[i].parse(ast, tokens);
+    }
+    fprintf(stderr, "command %s not found.\n", cmd.value.chars);
+    exit(1);
 }
 
 // create an entire ast
