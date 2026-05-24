154dabe01ad0a1534e049388081082e79c7781ea
nixii <161391079+nixii@users.noreply.github.com>
2026-05-24 15:51:29 -0700

    made the little test area

--- editor/main.c
+++ editor/main.c
@@ -1,16 +1,37 @@
 
 #include <stdio.h>
+#include <microgame/microgame.h>
 #include "lang/lexer.h"
 #include "lang/parser.h"
 #include "lang/interpret.h"
 
+#define WIDTH 1080
+#define HEIGHT 720
+#define FPS 60
+#define TITLE "Microgame!"
+
 int main(void) {
+
+    // create the game
+    microgame *game = game_new(WIDTH, HEIGHT, FPS, TITLE);
+
+    // the main scene
+    scene *s = scene_new();
+
+    // load the script
     ms_tokens tokens = tokenize("designing_the_engine/test.microscript");
-    // for (int i = 0; i < tokens.length; i++) {
-    //     printf("%d  ", tokens.data[i].type);
-    //     if (tokens.data[i].type == MS_TT_NEWLINE)
-    //         printf("\n");
-    // }
     ms_ast ast = parse(&tokens);
-    ms_interpreter interp = ms_interpreter_from(&ast, NULL, -1);
+
+    // interpret!
+    ms_interpreter interp = ms_interpreter_from(&ast, s, -1);
+
+    // run
+    while (game_still_running(game)) {
+        game_update(game);
+    }
+
+    // end
+    scene_destroy(s);
+    game_destroy(game);
+    return 0;
 }
